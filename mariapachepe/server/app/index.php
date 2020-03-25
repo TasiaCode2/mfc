476e7852b847f99940e48b6f1bf1817e8e66d61b
Ana castillo <escastillo.ana@outlook.com>
2020-03-25 12:47:52 -0600

    Mod index
    Intento arreglarlo

--- mariapachepe/server/app/index.php
+++ mariapachepe/server/app/index.php
@@ -1,11 +1,12 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	phpinfo()
+	/*if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 		$uri = 'https://';
 	} else {
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/dashboard/');
-	exit;
+	exit;*/
 ?>
 Something is wrong with the XAMPP installation :-(
